64855c854d6a452d6ac61e3db88bff7bc0f06e2b
Philipp Spitzley <kernel.familie.3w@icloud.com>
2025-02-04 16:49:43 +0100

    🛂 add dependencies for user and admin authentication

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,7 +1,14 @@
 from typing import Annotated
+
 from fastapi import Depends
 from sqlmodel import Session
 
 from .db import get_session
+from .utils.auth import get_current_admin, get_current_user
 
+# function dependencies
 SessionDep = Annotated[Session, Depends(get_session)]
+
+# path dependencies
+UserRequired = Depends(get_current_user)
+AdminRequired = Depends(get_current_admin)
