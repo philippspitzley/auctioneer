8da04d037d053e60cf3bf610c837e62b1fb6dc46
Philipp Spitzley <kernel.familie.3w@icloud.com>
2025-02-04 16:52:28 +0100

    🔧 update config with SECRET_KEY check
    - if SECRET_KEY is not set, raise RuntimeError

--- config.py
+++ config.py
@@ -6,16 +6,21 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # DB variables
-DB_USER = os.environ.get("DB_USER")
-DB_PW = os.environ.get("DB_PW")
-DB_HOST = os.environ.get("DB_HOST")
-DB_PORT = os.environ.get("DB_PORT")
-DB_NAME = os.environ.get("DB_NAME")
+DB_USER: str = os.environ.get("DB_USER", "")
+DB_PW: str = os.environ.get("DB_PW", "")
+DB_HOST: str = os.environ.get("DB_HOST", "")
+DB_PORT: str = os.environ.get("DB_PORT", "")
+DB_NAME: str = os.environ.get("DB_NAME", "")
 # construct DB_URI, if host is 'localhost' password is not required and automatically omits
 DB_URI = f"postgresql://{DB_USER}{'' if DB_HOST == 'localhost' else ':' + DB_PW}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 
 # Security variables
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY", None)
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+if not SECRET_KEY:
+    raise RuntimeError(
+        "SECRET_KEY is not set! Please configure it as an environment variable."
+    )
