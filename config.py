a3e54a38eeae9898196adb92a57376e8f532d203
Philipp Spitzley <kernel.familie.3w@icloud.com>
2025-02-05 17:50:16 +0100

    🔐 add ADMIN_USERNAME and ADMIN_PASSWORD and user warning when not set
    - reworked the warning messages for missing environment variables
    - warnings are styled to stand out and match the theme of fastapi

--- config.py
+++ config.py
@@ -1,9 +1,40 @@
 import os
+import warnings
+from colorama import Back, Style, Fore
 
 from dotenv import load_dotenv
 
 
 load_dotenv()
+
+# Security variables
+SECRET_KEY = os.environ.get("SECRET_KEY", None)
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+if not SECRET_KEY:
+    message = (
+        f"  {Style.BRIGHT}{Fore.WHITE}{Back.RED} WARNING {Style.RESET_ALL}  "
+        f"{Style.BRIGHT}{Fore.RED}SECRET_KEY{Style.NORMAL} is not set! "
+        "Please configure it as an environment variable."
+    )
+    print(message)
+
+
+# Admin variables
+# email is currently used for username
+ADMIN_USERNAME = os.environ.get("ADMIN_USERNAME", None)
+ADMIN_PASSWORD = os.environ.get("ADMIN_PASSWORD", None)
+
+if not ADMIN_USERNAME or not ADMIN_PASSWORD:
+    message = (
+        f"  {Style.BRIGHT}{Fore.WHITE}{Back.RED} WARNING {Style.RESET_ALL}  "
+        f"{Style.BRIGHT}{Fore.RED}ADMIN_USERNAME{Style.NORMAL} and "
+        f"{Style.BRIGHT}ADMIN_PASSWORD{Style.NORMAL} are not set! "
+        "Please configure them as environment variables."
+    )
+    print(message)
+
 
 # DB variables
 DB_USER: str = os.environ.get("DB_USER", "")
@@ -13,14 +44,3 @@ DB_PORT: str = os.environ.get("DB_PORT", "")
 DB_NAME: str = os.environ.get("DB_NAME", "")
 # construct DB_URI, if host is 'localhost' password is not required and automatically omits
 DB_URI = f"postgresql://{DB_USER}{'' if DB_HOST == 'localhost' else ':' + DB_PW}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-
-
-# Security variables
-SECRET_KEY = os.environ.get("SECRET_KEY", None)
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
-if not SECRET_KEY:
-    raise RuntimeError(
-        "SECRET_KEY is not set! Please configure it as an environment variable."
-    )
