f7a6edebea4d5adffde82dfd7e577864645a56dd
Philipp Spitzley <129886018+philippspitzley@users.noreply.github.com.>
2025-02-16 15:06:31 +0100

    ✨ add taskmanagment to fetch finished auctions
    - apscheduler is used to fetch periodically finished auctions
    in the background

---
+++ app/tasks.py
@@ -0,0 +1,22 @@
+from apscheduler.schedulers.background import (
+    BackgroundScheduler,  # runs tasks in the background
+)
+from apscheduler.triggers.interval import IntervalTrigger
+from sqlmodel import Session
+
+from . import utils
+from .routers.auctions import process_finished_auctions
+
+
+def process_finished_auctions_with_session():
+    utils.pretty_print("task", "Fetching finished auctions...")
+    with Session(utils.engine) as session:
+        process_finished_auctions(session)
+        session.commit()
+
+
+# Set up the scheduler
+scheduler = BackgroundScheduler()
+# trigger = CronTrigger(hour=13, minute=32)  # run every day on 13:32
+trigger_2 = IntervalTrigger(seconds=5)  # run every 2 minutes
+scheduler.add_job(process_finished_auctions_with_session, trigger_2)
