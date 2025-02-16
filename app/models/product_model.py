2da856e3ede132c7157ef3e48089226fcb5d2b2d
Philipp Spitzley <129886018+philippspitzley@users.noreply.github.com.>
2025-02-16 14:59:21 +0100

    🗃️ update product models
    - add new models
    - update datetime fields with timezone

--- app/models/product_model.py
+++ app/models/product_model.py
@@ -1,31 +1,57 @@
 from datetime import datetime
 from typing import TYPE_CHECKING, Optional
 
+import sqlalchemy as sa
 from sqlalchemy.orm import Mapped, relationship
 from sqlmodel import Field, Relationship, SQLModel
+
+from app.utils import get_current_timestamp
 
 # Import Product model only for type checking to avoid circular imports
 if TYPE_CHECKING:
     from .auction_model import Auction
+    from .user_model import User
 
-
+# TODO: change model to only show name and description
 # TODO: generate UUI
 # TODO: CRUD pydantic classes
 
 
+class ProductBase(SQLModel):
+    owner_id: int
+    name: str
+    description: str | None
+
+
+class ProductCreate(ProductBase):
+    pass
+
+
 class Product(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
+    owner_id: int = Field(
+        foreign_key="user.id",
+        ondelete="SET NULL",
+        nullable=True,
+    )
     name: str = Field(index=True)
     description: str | None = Field(default=None)
+    created_at: datetime | None = Field(
+        default_factory=get_current_timestamp,
+        sa_column=sa.Column(sa.DateTime(timezone=True)),
+    )
+    updated_at: datetime | None = Field(
+        default=None, sa_column=sa.Column(sa.DateTime(timezone=True))
+    )
 
-    created_at: datetime | None = Field(default=None)
-    updated_at: datetime | None = Field(default=None)
-
+    # Relationships
     categories: Mapped[list["Category"]] = Relationship(
         sa_relationship=relationship(back_populates="product")
     )
-
     auction: Optional["Auction"] = Relationship(
+        sa_relationship=relationship(back_populates="products")
+    )
+    owner: Optional["User"] = Relationship(
         sa_relationship=relationship(back_populates="products")
     )
 
@@ -35,13 +61,13 @@ class Product(SQLModel, table=True):
     # )
 
 
-# class ProductCreate(Product):
-#     created_at: datetime = Field(default_factory=get_current_timestamp)
+# class ProductPublic(Product):
+#     pass
 
 
 class Category(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
-    name: str = Field(index=True, unique=True)
+    # id: int | None = Field(default=None, primary_key=True)
+    name: str = Field(index=True, primary_key=True)
     description: str | None = Field(default=None)
 
     product_id: int | None = Field(
