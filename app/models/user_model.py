c347a4e95a91e5021ef942e1880369c6012b741f
Philipp Spitzley <129886018+philippspitzley@users.noreply.github.com.>
2025-02-16 15:00:38 +0100

    🗃️ update user models
    - update datetime fields with timezone
    - add products and sold_auctions relationships

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from enum import Enum
 from typing import TYPE_CHECKING
 
+import sqlalchemy as sa
 from pydantic import EmailStr
 from sqlalchemy.orm import Mapped, relationship
 from sqlmodel import Field, Relationship, SQLModel
@@ -11,6 +12,7 @@ from ..utils import get_current_timestamp
 # Import Product model only for type checking to avoid circular imports
 if TYPE_CHECKING:
     from .auction_model import Auction
+    from .product_model import Product
 
 
 # TODO: generate UUI
@@ -33,13 +35,31 @@ class UserPublic(UserBase):
 
 class User(UserPublic, table=True):
     password_hash: str
-    created_at: datetime = Field(default_factory=get_current_timestamp)
-    updated_at: datetime | None = Field(default=None)
+    created_at: datetime = Field(
+        default_factory=get_current_timestamp,
+        sa_column=sa.Column(sa.DateTime(timezone=True)),
+    )
+    updated_at: datetime | None = Field(
+        default=None,
+        sa_column=sa.Column(sa.DateTime(timezone=True)),
+    )
 
+    # Relationships
     auctions: Mapped[list["Auction"]] = Relationship(
         sa_relationship=relationship(
             back_populates="owner",
             foreign_keys="[Auction.owner_id]",
+        )
+    )
+    products: Mapped[list["Product"]] = Relationship(
+        sa_relationship=relationship(
+            back_populates="owner",
+        )
+    )
+    sold_auctions: Mapped[list["Auction"]] = Relationship(
+        sa_relationship=relationship(
+            back_populates="buyer",
+            foreign_keys="[Auction.buyer_id]",
         )
     )
 
