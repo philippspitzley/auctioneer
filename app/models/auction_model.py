cc0d84e772e591c906f6866bc84f8ef7d630e5d1
Philipp Spitzley <129886018+philippspitzley@users.noreply.github.com.>
2025-02-16 14:56:37 +0100

    🗃️ update auction and bid models
    - add new classes
    - update datetime fields with timezone

--- app/models/auction_model.py
+++ app/models/auction_model.py
@@ -2,19 +2,20 @@ from datetime import datetime
 from enum import Enum
 from typing import TYPE_CHECKING, Optional
 
+import sqlalchemy as sa
 from sqlalchemy.orm import Mapped, relationship
 from sqlmodel import Field, Relationship, SQLModel
 
-from ..models.product_model import Product
+# from ..models.product_model import Product
 from ..utils import get_current_timestamp
 
-# Import Product model only for type checking to avoid circular imports
+# Import Product and User model only for type checking to avoid circular imports
 if TYPE_CHECKING:
     from .product_model import Product
     from .user_model import User
 
-
-# TODO: generate UUI
+# TODO: handle delete of users, products and auction. How to handle foreign keys? Set null? cascade delete? set deleted username? keep auction and product. What if only owner or buyer is deleted?
+# # TODO: generate UUI
 
 
 class State(str, Enum):
@@ -24,13 +25,110 @@ class State(str, Enum):
 
 
 class AuctionBase(SQLModel):
-    owner_id: int = Field(foreign_key="user.id", ondelete="SET NULL")
+    owner_id: int
+    product_id: int
+
+
+class Auction(AuctionBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    owner_id: int = Field(
+        foreign_key="user.id",
+        ondelete="SET NULL",
+        nullable=True,
+    )
+    product_id: int | None = Field(
+        foreign_key="product.id",
+        ondelete="SET NULL",
+        nullable=True,
+    )
+    buyer_id: int | None = Field(
+        default=None,
+        foreign_key="user.id",
+        ondelete="SET NULL",
+        nullable=True,
+    )
     state: State = Field(default=State.setup)
+    start_time: datetime | None = Field(
+        default=None, sa_column=sa.Column(sa.DateTime(timezone=True))
+    )
+    end_time: datetime | None = Field(
+        default=None, sa_column=sa.Column(sa.DateTime(timezone=True))
+    )
+    starting_price: float | None = Field(default=None)
+    min_bid: float | None = Field(default=None)
+    instant_buy_price: float | None = Field(default=None)
+    sold: bool = Field(default=False)
+    sold_price: float | None = Field(default=None)
+    created_at: datetime = Field(
+        sa_column=sa.Column(sa.DateTime(timezone=True), nullable=False)
+    )
+    updated_at: datetime | None = Field(
+        default=None, sa_column=sa.Column(sa.DateTime(timezone=True))
+    )
+
+    # Relationships
+    bids: Mapped[list["Bid"]] = Relationship(
+        sa_relationship=relationship(back_populates="auction")
+    )
+    products: Mapped[list["Product"]] = Relationship(
+        sa_relationship=relationship(back_populates="auction")
+    )
+    owner: Optional["User"] = Relationship(
+        sa_relationship=relationship(
+            back_populates="auctions",
+            foreign_keys="[Auction.owner_id]",
+        )
+    )
+    buyer: Optional["User"] = Relationship(
+        sa_relationship=relationship(
+            back_populates="auctions",
+            foreign_keys="[Auction.buyer_id]",
+        )
+    )
+    # TODO: Due to a bug in sqlalchemy, the following line does not work
+    # bids: list[Bid] = Relationship(
+    #     back_populates="auction", cascade_delete=True
+    # )
+
+    # Methods
+    def has_ended(self) -> bool:
+        if self.state != State.live and self.end_time:
+            return self.end_time < get_current_timestamp()
+        return False
+
+    def get_highest_bidder(self):
+        if not self.bids:
+            return None
+        return max(self.bids, key=lambda bid: bid.amount)
+
+
+class AuctionCreate(AuctionBase):
+    starting_price: float | None = None
+    min_bid: float | None = None
+    instant_buy_price: float | None = None
+
+
+class AuctionCreateFromProduct(SQLModel):
+    starting_price: float | None = None
+    min_bid: float | None = None
+    instant_buy_price: float | None = None
+
+
+class AuctionUpdate(AuctionCreate):
+    owner_id: int | None = None
+    product_id: int | None = None
+    state: State | None = None
+    start_time: datetime | None = None
+    end_time: datetime | None = None
+    sold: bool | None = None
+    sold_price: float | None = None
+    buyer_id: int | None = None
 
 
 class AuctionPublic(AuctionBase):
-    id: int
-    product_id: int
+    id: int | None
+    state: State
+    product_id: int | None
     start_time: datetime | None
     end_time: datetime | None
     starting_price: float | None
@@ -41,66 +139,34 @@ class AuctionPublic(AuctionBase):
     sold_price: float | None
 
 
-class Auction(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
-    owner_id: int = Field(foreign_key="user.id")
-    product_id: int | None = Field(
-        foreign_key="product.id", ondelete="SET NULL"
-    )
-    state: State = Field(default=State.setup)
-    start_time: datetime | None = Field(default=None)
-    end_time: datetime | None = Field(default=None)
-    starting_price: float | None = Field(default=None)
-    min_bid: float | None = Field(default=None)
-    instant_buy_price: float | None = Field(default=None)
-    buyer_id: int | None = Field(default=None, foreign_key="user.id")
-    sold: bool = Field(default=False)
-    sold_price: float | None = Field(default=None)
-    created_at: datetime = Field(default_factory=get_current_timestamp)
-    updated_at: datetime | None = Field(default=None)
+class AuctionLive(AuctionBase):
+    id: int
+    owner_id: int
+    product_id: int
+    buyer_id: int | None
+    state: State = State.live
+    start_time: datetime
+    end_time: datetime
+    starting_price: float
+    min_bid: float | None
+    instant_buy_price: float | None
+    created_at: datetime
+    updated_at: datetime | None
 
-    bids: Mapped[list["Bid"]] = Relationship(
-        sa_relationship=relationship(back_populates="auction")
-    )
-
-    products: Mapped[list["Product"]] = Relationship(
-        sa_relationship=relationship(back_populates="auction")
-    )
-
-    owner: Optional["User"] = Relationship(
-        sa_relationship=relationship(
-            back_populates="auctions",
-            foreign_keys="[Auction.owner_id]",
-        )
-    )
-
-    # TODO: Due to a bug in sqlalchemy, the following line does not work
-    # bids: list[Bid] = Relationship(
-    #     back_populates="auction", cascade_delete=True
-    # )
-
-
-class AuctionCreate(AuctionBase):
-    pass
-
-
-class AuctionUpdate(SQLModel):
-    state: State | None = None
-    start_time: datetime | None = None
-    end_time: datetime | None = None
-    starting_price: float | None = None
-    min_bid: float | None = None
-    instant_buy_price: float | None = None
-    sold: bool | None = None
-    sold_price: float | None = None
-    updated_at: datetime = Field(default_factory=get_current_timestamp)
+    def has_ended(self) -> bool:
+        if self.state != State.live:
+            return self.end_time < get_current_timestamp()
+        return False
 
 
 class Bid(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     bidder_id: int = Field(foreign_key="user.id")
     amount: float
-    created_at: datetime = Field(default_factory=get_current_timestamp)
+    created_at: datetime = Field(
+        default_factory=get_current_timestamp,
+        sa_column=sa.Column(sa.DateTime(timezone=True)),
+    )
 
     auction_id: int | None = Field(
         foreign_key="auction.id", ondelete="CASCADE"
@@ -111,3 +177,7 @@ class Bid(SQLModel, table=True):
 
     # TODO: Due to a bug in sqlalchemy, the following line does not work
     # auction: Auction | None = Relationship(back_populates="bids")
+
+
+class BidCreate(SQLModel):
+    amount: float
